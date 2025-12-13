110d97db411da878c2e640b77d7b585bdaa2dbd3
Sofia Gonzalez Herrrera <sofia.gonzalezh@est.iudigital.edu.com>
2025-12-12 19:47:34 -0500

    configuracion firebase

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,17 @@
+import { getAnalytics } from "firebase/analytics";
+
+// Config usando variables del .env
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SEND,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+// Inicializamos Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
