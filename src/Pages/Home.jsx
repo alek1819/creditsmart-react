be6c842101abbd71617308eda8a573935172caad
Sofia Gonzalez Herrrera <sofia.gonzalezh@est.iudigital.edu.com>
2025-12-12 19:58:30 -0500

    enviar datos firebase al home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,30 @@
 import "../css/style.css";
 import logo from "../assets/images/creditsmart.svg";
 import Card from "../components/Card";
-import creditsData from "../data/creditsData";
 import Footer from "../components/Footer";
 
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfiguracion";
+
 export default function Home() {
+
+    const [credits, setCredits] = useState([]);
+
+    useEffect(() => {
+        const fetchCredits = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "creditos"));
+                const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setCredits(data);
+            } catch (error) {
+                console.error("Error obteniendo créditos:", error);
+            }
+        };
+
+        fetchCredits();
+    }, []);
+
     return (
         <>
             {/* Hero */}
@@ -20,18 +40,21 @@ export default function Home() {
                 <section className="credits-section">
                     <h3>Productos Crediticios</h3>
 
-                    {creditsData.map((credit, index) => (
-                        <Card
-                            key={index}
-                            icon={credit.icon}
-                            titulo={credit.titulo}
-                            descripcion={credit.descripcion}
-                            tasa={credit.tasa}
-                            monto={credit.monto}
-                            plazo={credit.plazo}
-                        />
-                    ))}
-
+                    {credits.length === 0 ? (
+                        <p>Cargando productos...</p>
+                    ) : (
+                        credits.map((credit) => (
+                            <Card
+                                key={credit.id}
+                                icon={credit.icon}
+                                titulo={credit.titulo}
+                                descripcion={credit.descripcion}
+                                tasa={credit.tasa}
+                                monto={credit.monto}
+                                plazo={credit.plazo}
+                            />
+                        ))
+                    )}
                 </section>
             </main>
 
